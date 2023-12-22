74727e1bad018ec78ca2d4117a3f50a9bade86b2
achaltelmasre <achaltelmasre@gmail.com>
2023-12-22 11:43:16 +0530

    initial commit

---
+++ server/model/Transaction.js
@@ -0,0 +1,11 @@
+import { Schema, model} from "mongoose";
+const transactionSchema = new Schema({
+    amount:{
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+})
