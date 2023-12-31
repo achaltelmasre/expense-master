bb40116bd5085045de8af7cf617fdd78a8567c2a
achaltelmasre <achaltelmasre@gmail.com>
2023-12-31 23:40:56 +0530

    added button in home page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer.js";
+import {Link} from "react-router-dom"
+
 import "./Home.css";
-// import Footer from "../../components/Footer/Footer";
 
 
 function Home () {
@@ -16,11 +16,18 @@ function Home () {
 
                 <h2 className="text-light ms-5">The Most Convenient & Easiest Way to Track Your Money</h2>
 
-                <h6 className="text-light fs-4 ms-5 mt-5  text ">This app helps you understand your money better. Whether you want to see where your money goes each month or keep track of where you earn, this app gives you clear information. You can organize your transactions by date or amount, so you can easily see and understand your financial information in the way that's easiest for you.</h6>
+                <h6 className="text-light fs-4 ms-5 mt-5  text  ">This app helps you understand your money better. Whether you want to see where your money goes each month or keep track of where you earn, this app gives you clear information. You can organize your transactions by date or amount, so you can easily see and understand your financial information in the way that's easiest for you.</h6>
+
+
+                <Link to="/addtransaction">
+                    <button type="button" class="btn btn-danger ms-5 mt-3 m-5 fs-4 button " >Add Transaction ➡️</button>
+                </Link>
 
              </div>
+
+            
+
              
-             <Footer/>
 
          </div>
 
